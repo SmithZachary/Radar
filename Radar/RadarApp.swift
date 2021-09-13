165ee2cd918464d671b01a2dba9a4015fea86c21
SmithZachary <89857357+SmithZachary@users.noreply.github.com>
2021-09-13 16:48:22 -0500

    Initial Commit

---
+++ Radar/RadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadarApp.swift
+//  Radar
+//
+//  Created by Zach Smith on 9/13/21.
+//
+
+import SwiftUI
+
+@main
+struct RadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
